d89412ecb0f992a35c314fef157f6d6d66e65a59
rootmap <f.bhuyian@gmail.com>
2021-08-30 22:28:18 +0600

    Init

---
+++ aboutus.php
@@ -0,0 +1,68 @@
+<?php include_once 'partials/header.php'; ?>
+<!-- Chef section Start -->
+<section class="chef">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4">
+				<div class="ser_cs">
+					<div class="row">
+					<div class="exservice">
+                        <div class="chef_service">
+                            <div class="s-box">
+                                <div class="service-image">
+                                    <a href="javascript:void(0)">
+                                        <img src="image/s-image01.png" class="img-fluid" alt="image">
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+				</div>
+				<div class="row">
+					<div class="p_section myBox">
+						<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis inventore obcaecati facilis reiciendis dolor vero delectus quos adipisci deleniti aut saepe, eius, nostrum omnis necessitatibus quasi. Impedit similique repellendus dignissimos. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat dicta dolorum pariatur maiores, accusantium in nesciunt adipisci excepturi velit exercitationem eos suscipit. Itaque delectus quos sapiente nesciunt expedita dignissimos veniam?</p>
+					</div>
+				</div>
+				</div>
+			</div>
+			<div class="col-lg-8">
+				<div class="ser_csi">
+					<div class="row">
+					<div class="col-lg-7">
+						<p class="header">S Rahman</p>
+						<div class="p_section">
+							<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium praesentium, odit voluptatem mollitia iure doloribus sed est aut rerum ex recusandae modi quidem debitis nemo soluta, dolores voluptatum dolore obcaecati.</p>
+						</div>
+					</div>
+					<div class="col-lg-5">
+						<div class="img_box">
+							<img src="image/Chef/5.jpg" alt="">
+						</div>
+					</div>
+				</div><hr>
+				<div class="row">
+					<div class="col-lg-7">
+						<p class="header">S Rahman</p>
+						<div class="p_section">
+							<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium praesentium, odit voluptatem mollitia iure doloribus sed est aut rerum ex recusandae modi quidem debitis nemo soluta, dolores voluptatum dolore obcaecati.</p>
+						</div>
+					</div>
+					<div class="col-lg-5">
+						<div class="img_box">
+							<img src="image/Chef/5.jpg" alt="">
+						</div>
+					</div>
+				</div>
+				</div>				
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- Chef section End -->
+<?php include_once 'partials/footer.php'; ?>
+</body>
+
+</html>
+
+</html>
---
+++ partials/gallery_light_box.php
@@ -0,0 +1,119 @@
+<section class="quick-view">
+	<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLabel">Product Quick View</h5>
+					<a href="javascript:void(0)" data-bs-dismiss="modal" aria-label="Close"><i class="ion-close-round"></i></a>
+				</div>
+				<div class="quick-veiw-area">
+					<div class="quick-image">
+						<div class="tab-content">
+							<div class="tab-pane fade show active" id="image-1">
+								<a href="javascript:void(0)" class="long-img">
+									<img src="image/pro-page-image/pro-page-image.jpg" class="img-fluid" alt="image">
+								</a>
+							</div>
+							<div class="tab-pane fade show" id="image-2">
+								<a href="javascript:void(0)" class="long-img">
+									<img src="image/pro-page-image/3.jpg" class="img-fluid" alt="image">
+								</a>
+							</div>
+							<div class="tab-pane fade show" id="image-3">
+								<a href="javascript:void(0)" class="long-img">
+									<img src="image/pro-page-image/4.jpg" class="img-fluid" alt="image">
+								</a>
+							</div>
+							<div class="tab-pane fade show" id="image-4">
+								<a href="javascript:void(0)" class="long-img">
+									<img src="image/pro-page-image/5.jpg" class="img-fluid" alt="image">
+								</a>
+							</div>
+							<div class="tab-pane fade show" id="image-5">
+								<a href="javascript:void(0)" class="long-img">
+									<img src="image/pro-page-image/6.jpg" class="img-fluid" alt="image">
+								</a>
+							</div>
+							<div class="tab-pane fade show" id="image-6">
+								<a href="javascript:void(0)" class="long-img">
+									<img src="image/pro-page-image/7.jpg" class="img-fluid" alt="image">
+								</a>
+							</div>
+							<div class="tab-pane fade show" id="image-7">
+								<a href="javascript:void(0)" class="long-img">
+									<img src="image/pro-page-image/8.jpg" class="img-fluid" alt="image">
+								</a>
+							</div>
+							<div class="tab-pane fade show" id="image-8">
+								<a href="javascript:void(0)" class="long-img">
+									<img src="image/pro-page-image/8.jpg" class="img-fluid" alt="image">
+								</a>
+							</div>
+						</div>
+						<ul class="nav nav-tabs quick-slider owl-carousel owl-theme">
+							<li class="nav-item items">
+								<a class="nav-link active" data-bs-toggle="tab" href="#image-1"><img src="image/pro-page-image/pro-page-image.jpg" class="img-fluid" alt="image"></a>
+							</li>
+							<li class="nav-item items">
+								<a class="nav-link" data-bs-toggle="tab" href="#image-2"><img src="image/pro-page-image/3.jpg" class="img-fluid" alt="iamge"></a>
+							</li>
+							<li class="nav-item items">
+								<a class="nav-link" data-bs-toggle="tab" href="#image-3"><img src="image/pro-page-image/4.jpg" class="img-fluid" alt="image"></a>
+							</li>
+							<li class="nav-item items">
+								<a class="nav-link" data-bs-toggle="tab" href="#image-4"><img src="image/pro-page-image/5.jpg" class="img-fluid" alt="image"></a>
+							</li>
+							<li class="nav-item items">
+								<a class="nav-link" data-bs-toggle="tab" href="#image-5"><img src="image/pro-page-image/6.jpg" class="img-fluid" alt="image"></a>
+							</li>
+							<li class="nav-item items">
+								<a class="nav-link" data-bs-toggle="tab" href="#image-6"><img src="image/pro-page-image/7.jpg" class="img-fluid" alt="image"></a>
+							</li>
+							<li class="nav-item items">
+								<a class="nav-link" data-bs-toggle="tab" href="#image-7"><img src="image/pro-page-image/8.jpg" class="img-fluid" alt="image"></a>
+							</li>
+							<li class="nav-item items">
+								<a class="nav-link" data-bs-toggle="tab" href="#image-8"><img src="image/pro-page-image/9.jpg" class="img-fluid" alt="image"></a>
+							</li>
+						</ul>
+					</div>
+					<div class="quick-caption">
+						<h4>Fresh organic reachter</h4>
+						<div class="quick-price">
+							<span class="new-price">$350.00 USD</span>
+							<span class="old-price"><del>$399.99 USD</del></span>
+						</div>
+						<div class="quick-rating">
+							<i class="fa fa-star c-star"></i>
+							<i class="fa fa-star c-star"></i>
+							<i class="fa fa-star c-star"></i>
+							<i class="fa fa-star-o"></i>
+							<i class="fa fa-star-o"></i>
+						</div>
+						<div class="pro-description">
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and</p>
+						</div>
+						<div class="pro-size">
+							<label>Size: </label>
+							<select>
+								<option>1 ltr</option>
+								<option>3 ltr</option>
+								<option>5 ltr</option>
+							</select>
+						</div>
+						<div class="plus-minus">
+							<span>
+								<a href="javascript:void(0)" class="minus-btn text-black">-</a>
+								<input type="text" name="name" value="1">
+								<a href="javascript:void(0)" class="plus-btn text-black">+</a>
+							</span>
+							<a href="cart.html" class="quick-cart"><i class="fa fa-shopping-bag"></i></a>
+							<a href="wishlist.html" class="quick-wishlist"><i class="fa fa-heart"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
